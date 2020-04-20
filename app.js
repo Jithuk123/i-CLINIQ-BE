a8b87baa845eb14cbd8cedfe4297a9a249cc5554
Jithuk123 <jithujose@experionglobal.com>
2020-04-20 15:33:38 +0530

    added

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //d added
 app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// e added
+//e addedddd dd
 //importing routing file
 // require('./router')(app);
 // catch 404 and forward to error handler
